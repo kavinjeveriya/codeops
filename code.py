22ad9212ba511539efd1c7bdd26a8fe38c88ca34
kavinjeveriya <kavinjaveriya@gmail.com>
2021-06-30 21:24:48 +0530

    my data set

--- code.py
+++ code.py
@@ -24,8 +24,8 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 from keras_preprocessing.image import ImageDataGenerator
 traingen = ImageDataGenerator(rescale=1./255 , zoom_range=0.2 , horizontal_flip=True ,  vertical_flip=True )
 testgen = ImageDataGenerator(rescale=1./255)
-trainset = traingen.flow_from_directory('/codeops/cnn_dataset/training_set/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
-testset = testgen.flow_from_directory('/codeops/cnn_dataset/test_set/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
+trainset = traingen.flow_from_directory('/code/cnn_dataset/training_set/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
+testset = testgen.flow_from_directory('/code/cnn_dataset/test_set/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
 model.fit(trainset , epochs=3 , validation_data=testset , steps_per_epoch=9259, validation_steps=10  )
 model.save("cnnmlops.h5")
 scores = model.evaluate(testset,verbose=1)
